e4b200c5fdfe916f4cff4776d38b55d801df6d41
Niv Ben-David <nivbend@gmail.com>
2020-02-21 12:26:50 +0200

    Add the `in_branch` and `restore_head` utilities
    `restore_head` allows us to make modifications inside a context and
    always return to the previous head when done.
    
    The `in_branch` decorator allows making modifications inside a function
    on a new branch and returning to the previous branch (probably `master`)
    when done.

--- src/gitstery/git_utils.py
+++ src/gitstery/git_utils.py
@@ -1,3 +1,5 @@
+from functools import wraps
+from contextlib import contextmanager
 from .utils import wrap_paragraphs
 
 def git_commit(repo, author, date, title, body=''):
@@ -9,3 +11,22 @@ def git_commit(repo, author, date, title, body=''):
         author_date=date.isoformat(),
         committer=author.actor,
         commit_date=date.isoformat())
+
+@contextmanager
+def restore_head(repo):
+    prev_head = repo.head.reference
+    try:
+        yield
+    finally:
+        prev_head.checkout()
+
+def in_branch(name):
+    def _in_branch_decorator(func):
+        @wraps(func)
+        def _switch_branches(repo, *args, **kwargs):
+            with restore_head(repo):
+                new_branch = repo.create_head(name)
+                new_branch.checkout()
+                func(repo, *args, **kwargs)
+        return _switch_branches
+    return _in_branch_decorator
