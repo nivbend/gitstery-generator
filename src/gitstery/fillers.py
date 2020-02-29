d3fde7a48fc88b032debc9ed49b13500cd99e3cd
Niv Ben-David <nivbend@gmail.com>
2020-02-29 09:17:58 +0200

    Add random paragraphs generator
    To fill "fluff" commit messages we copy parts of Arthur Conan Doyle's
    "The Adventues of Sherlock Holmes." According to Wikisource this work is
    public domain because it was published prior to January 1st, 1925. So
    using it should be fine.
    
    The actual work of chaining paragraphs required some text manipulation.
    First, we needed to dedent the entire thing. Then we dropped paragraphs
    that started with a whitespace because those were story titles. Then we
    drop chapter titles. Last we dropped the first 50 paragraphs to get rid
    of the table of contents and parts of the exposition. This should
    provide us with commit messages that seem more "natural."

--- src/gitstery/fillers.py
+++ src/gitstery/fillers.py
@@ -1,4 +1,6 @@
-from random import choice
+from itertools import islice
+from random import choice, randrange
+from textwrap import dedent
 from .defines import DATA_DIR
 from .people import Person
 
@@ -18,3 +20,25 @@ def random_people(people_of_interest):
         email = f'{given_name[0].lower()}{surname.lower()}@{choice(providers)}'
         yield Person(f'{given_name} {surname}', email)
         people.add((given_name, surname))
+
+def random_paragraphs(count = -1):
+    global _paragraphs
+    if not _paragraphs:
+        # Load book for random paragraphs.
+        book_path = DATA_DIR / 'book.txt'
+        with book_path.open('r') as book:
+            # Split the book by paragraphs, we drop the first 50 "paragraphs" to skip the title, TOC,
+            # and parts of the exposition. We also drop "empty" paragraph, stories and chapter titles.
+            # No need to wrap the paragraph right now, `git_commit` takes care of that.
+            _paragraphs = (' '.join(dedent(p).splitlines()) for p in book.read().split('\n\n'))
+            _paragraphs = [p for p in islice(_paragraphs, 50, None)
+                if p and not (p.startswith(' ') or p.startswith('CHAPTER'))]
+
+    if count is None:
+        return choice(_paragraphs)
+    if count <= 0:
+        count = 2 + randrange(5)
+    index = randrange(len(_paragraphs))
+    return '\n\n'.join(_paragraphs[index:index + count])
+
+_paragraphs = None
